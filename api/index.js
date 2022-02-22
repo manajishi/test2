db21ea191ecfd0066ea1de7b452741dfca2ea5ab
manaya <manaya1103@icloud.com>
2022-02-22 17:50:51 +0900

    Init: project

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', function (req, res){
- res.send("hello world");
-});
 
+//Declare the json.
+const to_json=[{ Name :"Manaya",Gender :"Man"}]
+const json_string = JSON.stringify(to_json);
+
+app.get('/', function (req, res){
+ res.send(json_string);
+});
 
 //app.listen(3000, function () {
 //});
