2aaa4a9f7c78a5d85be920d045452fa5184d888e
manaya <manaya1103@icloud.com>
2022-02-22 19:25:10 +0900

    Init: project

--- api/index.js
+++ api/index.js
@@ -4,18 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //JS obj to json
-//const to_json=[{ Name :"Manaya",Gender :"Man"},
-//{ Name :"Manuel",Gender :"Man"}]
-//const json_string = JSON.stringify(to_json);
+const to_json=[{ Name :"Manaya",Gender :"Man"},
+{ Name :"Manuel",Gender :"Man"}]
+const json_string = JSON.stringify(to_json);
 //json to JS obj
-//const from_json = JSON.parse(json_string);
+const from_json = JSON.parse(json_string);
 
 app.get('/', function (req, res){
-
-  $.getJSON(manaya.json, (data) => {
-    console.log(`id=${data.id}, name=${data.name}, email=${data.email}`);});
-    
- //res.send(from_json);
+ res.send(from_json[0].Name);
 });
 
 //check
