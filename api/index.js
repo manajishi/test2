b4ed8bec0af9d8e76fa841a309d1817c5d0f0061
manaya <manaya1103@icloud.com>
2022-02-22 17:07:15 +0900

    Init: project

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', function (req, res) {
- res.send('Hello World!');
+app.get('/', function (req, res){
+ res.json();
 });
 
 //app.listen(3000, function () {
