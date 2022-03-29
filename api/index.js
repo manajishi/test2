bfaea9898020458c296c251cf694a8954d2edc63
manaya <manaya1103@icloud.com>
2022-03-29 19:43:56 +0900

    Init: project

--- api/index.js
+++ api/index.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//JS obj to json
-const to_json=[{ Name :"Manaya",Gender :"Man"},
-{ Name :"Manuel",Gender :"Man"}]
-const json_string = JSON.stringify(to_json);
-//json to JS obj
-const from_json = JSON.parse(json_string);
+app.use(bodyParser.json());
 
-app.get('/', function (req, res){
- res.send(from_json[0].Name);
+let yo;
+app.post("/api/get", (req, res) => {
+  yo = req.body;
+  console.log(yo);
 });
 
-//check
-// console.log("Start");
- //console.log(from_json[0].Name);
- //console.log(data);
-//app.listen(3000, function () {
-//});
-(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT); 
+app.post('/', function (req, res) {
+  // console.log(req.body);
+   const data = req.body;
+   console.log('req.body', data);
+   res.send('api: OK');
+  });
+  
+//なくてよい
+app.post("/", (req, res) => {
+  const data = req.body;
+  console.log("req.body", data);
+  const messageID = data["events"][0]["message"]["id"];
+  console.log("messageID:", messageID);
+  res.send("api:OK");
+});
 
+//app.listen(3000, () => console.log("Listening on port 3000"));
+process.env.NOW_REGION
+  ? (module.exports = app)
+  : app.listen(PORT, () => console.log(`now listening on port${PORT}`));
  
